46b527b800af3368a87787bdad6e29013f117089
Andres <andresnei0820@gmail.com>
2023-12-16 15:49:03 -0500

    first component

---
+++ vite-project/src/MyComponent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Mycomponent = () => {
+    return (
+        <>
+        <div className="first-comp">
+            <h2>Hello guys</h2>
+                    <li>number 1</li>
+                    <li>number 2</li>
+                    <li>number 3</li>
+        </div>
+        </>
+    )
+}
+
+export default Mycomponent
