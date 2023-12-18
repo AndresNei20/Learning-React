733d4cef7f856b594cefde409ceb2865fc139648
Andres <andresnei0820@gmail.com>
2023-12-18 15:59:59 -0500

    prop types and prop validation

--- vite-project/src/ThirdComponent.jsx
+++ vite-project/src/ThirdComponent.jsx
@@ -1,23 +1,28 @@
 import React from "react";
+import Proptypes from "prop-types"
 
-const ThirdComponent = (props) => {
-
-    console.log(props)
+const ThirdComponent = ({name, last, userCard}) => {
 
     return(
         <div className="third-component">
             <h3>Component to Learn component comunication</h3>
             <ul>
-                <li>{props.name}</li>
-                <li>{props.last}</li>
-                <li>{props.userCard.height}</li>
-                <li>{props.userCard.weight}</li>
-                <li>{props.userCard.blood}</li>
-                <li>{props.userCard.state}</li>
+                <li>{name}</li>
+                <li>{last}</li>
+                <li>{userCard.height}</li>
+                <li>{userCard.weight}</li>
+                <li>{userCard.blood}</li>
+                <li>{userCard.state}</li>
 
             </ul>
         </div>
     )
+}
+
+ThirdComponent.propTypes = {
+    nombre: Proptypes.string.isRequired, //Proptypes.string.isRequired  this mean that the prop must to have content
+    last: Proptypes.string , //if this prop doesnt come with any data, this going to be void or undefined
+    userCard: Proptypes.object
 }
 
 export default ThirdComponent
