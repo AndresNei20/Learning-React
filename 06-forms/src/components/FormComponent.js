2c4e25a9170904e147c126413d43f514c2462e85
Andres <andresnei0820@gmail.com>
2023-12-28 16:55:43 -0500

    forms and submit info object

--- 06-forms/src/components/FormComponent.js
+++ 06-forms/src/components/FormComponent.js
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const FormComponent = () => {
+
+  const [user, setUser] = useState({});
+
+  const getDataForm = e => {
+    e.preventDefault(); //this dont reload the page
+
+    let data = e.target;
+    console.log(data.name.value)
+    let user = {
+      name:data.name.value,
+      last:data.last.value,
+      gender:data.gender.value,
+      bio: data.bio.value
+    }
+
+    console.log(user)
+
+    setUser(user)
+  }
+
+
   return (
     <div className='form-component'>
       <h1>Form</h1>
-
-      <form>
-            <input type='text' placeholder='name'></input>
-            <input type='text' placeholder='last'></input>
-            <select>
+      
+       {user.bio && user.bio.lenght >= 1 &&
+       
+          <div className='user-info-label'>
+            {user.name} {user.last} is a {user.gender} and this is its bio: <p> {user.bio}</p>
+          </div>
+      }
+      <form onSubmit={getDataForm}>
+            <input type='text' placeholder='name' name="name"></input>
+            <input type='text' placeholder='last' name="last"></input>
+            <select name='gender'>
                 <option value="male">Male</option>
                 <option value="female">Female</option>
             </select>
-            <textarea placeholder='Bio'></textarea>
+            <textarea placeholder='Bio' name='bio'></textarea>
             <input type='submit' value="Send"></input>
 
       </form>
