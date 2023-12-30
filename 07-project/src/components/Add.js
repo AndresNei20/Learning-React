032e0acd90680d22f06869fdd562cd6c99b1a002
Andres <andresnei0820@gmail.com>
2023-12-30 17:00:17 -0500

    created form and functions

--- 07-project/src/components/Add.js
+++ 07-project/src/components/Add.js
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Add = () => {
+
+    const titleComponent = "Add Mmovie"
+    const [movieState, setMovieState] = useState({
+        title: "",
+        description:""
+    })
+
+    const {title, description} = movieState
+
+    const getFormData = e => {
+        e.preventDefault()
+
+        let target = e.target;
+        let title = target.title.value
+        let description = target.description.value
+
+        let movie = {
+            id:new Date().getTime(),
+            title,
+            description
+        }
+        
+        setMovieState(movie)
+
+        console.log(movie)
+
+    }
+
   return (
     <div className="add">
-      <h3 className="title">Add movie</h3>
-      <form action="">
-        <input type="text" aria-placeholder="Title" />
-        <textarea placeholder="Description"></textarea>
-        <input type="submit" />
+      <h3 className="title">{titleComponent}</h3>
+
+    <strong>
+     {(title && description) && "You created the movie: " + title}
+    </strong>
+
+      <form onSubmit={getFormData}>
+        <input type="text" placeholder="Title" id="title" name="title"/>
+        <textarea id="description" name="description" placeholder="Description"></textarea>
+        <input id="save" value="Save" type="submit" />
       </form>
     </div>
   );
